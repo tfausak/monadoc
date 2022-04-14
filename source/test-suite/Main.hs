d6f0d0e6bf3af917b6d2811a98aba1d0ffbaca12
Taylor Fausak <taylor@fausak.me>
2022-04-14 18:28:54 +0000

    Emit easy to consume failure messages

--- source/test-suite/Main.hs
+++ source/test-suite/Main.hs
@@ -1,90 +1,41 @@
-import qualified Monadoc.Action.Blob.UpsertSpec
-import qualified Monadoc.Action.Database.InitializeSpec
-import qualified Monadoc.Action.Database.VacuumSpec
-import qualified Monadoc.Action.HackageUser.UpsertSpec
-import qualified Monadoc.Action.Job.EnqueueSpec
-import qualified Monadoc.Action.Key.SelectLastInsertSpec
-import qualified Monadoc.Action.Package.UpsertSpec
-import qualified Monadoc.Action.Preference.UpsertSpec
-import qualified Monadoc.Action.Upload.UpsertSpec
-import qualified Monadoc.Action.Version.UpsertSpec
-import qualified Monadoc.Extra.DirectSqliteSpec
-import qualified Monadoc.Extra.EitherSpec
-import qualified Monadoc.Extra.TimeSpec
-import qualified Monadoc.Model.BlobSpec
-import qualified Monadoc.Model.CronEntrySpec
-import qualified Monadoc.Model.HackageIndexSpec
-import qualified Monadoc.Model.HackageUserSpec
-import qualified Monadoc.Model.JobSpec
-import qualified Monadoc.Model.MigrationSpec
-import qualified Monadoc.Model.PackageSpec
-import qualified Monadoc.Model.PreferenceSpec
-import qualified Monadoc.Model.UploadSpec
-import qualified Monadoc.Model.VersionSpec
-import qualified Monadoc.Type.ConfigSpec
-import qualified Monadoc.Type.ConstraintSpec
-import qualified Monadoc.Type.FlagSpec
-import qualified Monadoc.Type.GuidSpec
-import qualified Monadoc.Type.HackageUserNameSpec
-import qualified Monadoc.Type.HashSpec
-import qualified Monadoc.Type.KeySpec
-import qualified Monadoc.Type.ModelSpec
-import qualified Monadoc.Type.PackageNameSpec
-import qualified Monadoc.Type.PortSpec
-import qualified Monadoc.Type.RevisionSpec
-import qualified Monadoc.Type.RouteSpec
-import qualified Monadoc.Type.ScheduleSpec
-import qualified Monadoc.Type.SeveritySpec
-import qualified Monadoc.Type.StatusSpec
-import qualified Monadoc.Type.TaskSpec
-import qualified Monadoc.Type.TimestampSpec
-import qualified Monadoc.Type.VersionNumberSpec
-import qualified Test.Hspec as Hspec
+import qualified Control.Monad as Monad
+import qualified Monadoc.Spec as Monadoc
+import qualified System.Environment as Environment
+import qualified Test.Hspec.Core.Formatters.V2 as Formatter
+import qualified Test.Hspec.Core.Spec as Spec
+import qualified Test.Hspec.Core.Util as Util
+import qualified Test.Hspec.Runner as Runner
+import qualified Text.Printf as Printf
 
 main :: IO ()
-main = Hspec.hspec spec
+main = do
+  arguments <- Environment.getArgs
+  config <- Runner.readConfig defaultConfig arguments
+  summary <- Environment.withArgs [] $ Runner.runSpec Monadoc.spec config
+  Runner.evaluateSummary summary
 
-spec :: Hspec.Spec
-spec = do
-  Hspec.it "TODO" $ False `Hspec.shouldBe` True
-  Monadoc.Action.Blob.UpsertSpec.spec
-  Monadoc.Action.Database.InitializeSpec.spec
-  Monadoc.Action.Database.VacuumSpec.spec
-  Monadoc.Action.HackageUser.UpsertSpec.spec
-  Monadoc.Action.Job.EnqueueSpec.spec
-  Monadoc.Action.Key.SelectLastInsertSpec.spec
-  Monadoc.Action.Package.UpsertSpec.spec
-  Monadoc.Action.Preference.UpsertSpec.spec
-  Monadoc.Action.Upload.UpsertSpec.spec
-  Monadoc.Action.Version.UpsertSpec.spec
-  Monadoc.Extra.DirectSqliteSpec.spec
-  Monadoc.Extra.EitherSpec.spec
-  Monadoc.Extra.TimeSpec.spec
-  Monadoc.Model.BlobSpec.spec
-  Monadoc.Model.CronEntrySpec.spec
-  Monadoc.Model.HackageIndexSpec.spec
-  Monadoc.Model.HackageUserSpec.spec
-  Monadoc.Model.JobSpec.spec
-  Monadoc.Model.MigrationSpec.spec
-  Monadoc.Model.PackageSpec.spec
-  Monadoc.Model.PreferenceSpec.spec
-  Monadoc.Model.UploadSpec.spec
-  Monadoc.Model.VersionSpec.spec
-  Monadoc.Type.ConfigSpec.spec
-  Monadoc.Type.ConstraintSpec.spec
-  Monadoc.Type.FlagSpec.spec
-  Monadoc.Type.GuidSpec.spec
-  Monadoc.Type.HackageUserNameSpec.spec
-  Monadoc.Type.HashSpec.spec
-  Monadoc.Type.KeySpec.spec
-  Monadoc.Type.ModelSpec.spec
-  Monadoc.Type.PackageNameSpec.spec
-  Monadoc.Type.PortSpec.spec
-  Monadoc.Type.RevisionSpec.spec
-  Monadoc.Type.RouteSpec.spec
-  Monadoc.Type.ScheduleSpec.spec
-  Monadoc.Type.SeveritySpec.spec
-  Monadoc.Type.StatusSpec.spec
-  Monadoc.Type.TaskSpec.spec
-  Monadoc.Type.TimestampSpec.spec
-  Monadoc.Type.VersionNumberSpec.spec
+defaultConfig :: Runner.Config
+defaultConfig =
+  Runner.defaultConfig
+    { Runner.configFormat =
+        Just $
+          Formatter.formatterToFormat
+            Formatter.checks
+              { Formatter.formatterDone = do
+                  done
+                  Formatter.formatterDone Formatter.checks
+              }
+    }
+
+done :: Formatter.FormatM ()
+done = do
+  failures <- Formatter.getFailMessages
+  Monad.when (not $ null failures) $ Formatter.writeLine ""
+  Monad.forM_ failures $ \failure ->
+    Formatter.writeLine $
+      Printf.printf
+        "FAILURE file %s line %d column %d path %s"
+        (maybe "unknown" Spec.locationFile $ Formatter.failureRecordLocation failure)
+        (maybe (-1) Spec.locationLine $ Formatter.failureRecordLocation failure)
+        (maybe (-1) Spec.locationColumn $ Formatter.failureRecordLocation failure)
+        (Util.formatRequirement $ Formatter.failureRecordPath failure)
