0832448dd61c4dd0d8efd0ad9e0d17ed3bee5aee
Taylor Fausak <taylor@fausak.me>
2022-04-14 21:09:12 -0400

    Update test output format for CI

--- source/test-suite/Main.hs
+++ source/test-suite/Main.hs
@@ -1,41 +1,107 @@
 import qualified Control.Monad as Monad
+import qualified Control.Monad.Catch as Exception
+import qualified Data.Maybe as Maybe
+import qualified Data.Typeable as Typeable
 import qualified Monadoc.Spec as Monadoc
 import qualified System.Environment as Environment
-import qualified Test.Hspec.Core.Formatters.V2 as Formatter
-import qualified Test.Hspec.Core.Spec as Spec
-import qualified Test.Hspec.Core.Util as Util
+import qualified Test.Hspec.Core.Format as Format
+import qualified Test.Hspec.Core.Formatters as Formatters
 import qualified Test.Hspec.Runner as Runner
-import qualified Text.Printf as Printf
 
 main :: IO ()
 main = do
   arguments <- Environment.getArgs
-  config <- Runner.readConfig defaultConfig arguments
-  summary <- Environment.withArgs [] $ Runner.runSpec Monadoc.spec config
+  config <- Runner.readConfig Runner.defaultConfig arguments
+  summary <- Environment.withArgs [] . Runner.runSpec Monadoc.spec $ appendCiFormat config
   Runner.evaluateSummary summary
 
-defaultConfig :: Runner.Config
-defaultConfig =
-  Runner.defaultConfig
-    { Runner.configFormat =
-        Just $
-          Formatter.formatterToFormat
-            Formatter.checks
-              { Formatter.formatterDone = do
-                  done
-                  Formatter.formatterDone Formatter.checks
-              }
+appendCiFormat :: Runner.Config -> Runner.Config
+appendCiFormat config = withFormat (getFormat config <> ciFormatter) config
+
+withFormat :: (Format.FormatConfig -> IO Format.Format) -> Runner.Config -> Runner.Config
+withFormat format config =
+  config
+    { Runner.configFormat = Just format,
+      Runner.configFormatter = Nothing
     }
 
-done :: Formatter.FormatM ()
-done = do
-  failures <- Formatter.getFailMessages
-  Monad.when (not $ null failures) $ Formatter.writeLine ""
-  Monad.forM_ failures $ \failure ->
-    Formatter.writeLine $
-      Printf.printf
-        "FAILURE %s:%d:%d: %s"
-        (maybe "?" Spec.locationFile $ Formatter.failureRecordLocation failure)
-        (maybe 0 Spec.locationLine $ Formatter.failureRecordLocation failure)
-        (maybe 0 Spec.locationColumn $ Formatter.failureRecordLocation failure)
-        (Util.formatRequirement $ Formatter.failureRecordPath failure)
+getFormat :: Runner.Config -> Format.FormatConfig -> IO Format.Format
+getFormat = Maybe.fromMaybe defaultFormat . lookupFormat
+
+lookupFormat :: Runner.Config -> Maybe (Format.FormatConfig -> IO Format.Format)
+lookupFormat config = case Runner.configFormat config of
+  Just f -> Just f
+  Nothing -> case Runner.configFormatter config of
+    Just x -> Just $ Formatters.formatterToFormat x
+    Nothing -> Nothing
+
+defaultFormat :: Format.FormatConfig -> IO Format.Format
+defaultFormat = Formatters.formatterToFormat Formatters.checks
+
+ciFormatter :: Applicative io => formatConfig -> io Format.Format
+ciFormatter = const $ pure ciFormat
+
+ciFormat :: Format.Format
+ciFormat = whenDone $ \xs -> whenCi $ do
+  putStrLn ""
+  mapM_ (putStrLn . uncurry formatFailure)
+    . Maybe.mapMaybe toFailure
+    $ fmap (Format.itemResult . snd) xs
+
+whenDone ::
+  Applicative io =>
+  ([(Runner.Path, Format.Item)] -> io ()) ->
+  Format.Event ->
+  io ()
+whenDone callback event = case event of
+  Format.Done xs -> callback xs
+  _ -> pure ()
+
+whenCi :: IO () -> IO ()
+whenCi action = do
+  ci <- Environment.lookupEnv "CI"
+  Monad.when (ci == Just "true") action
+
+toFailure :: Format.Result -> Maybe (Maybe Format.Location, Format.FailureReason)
+toFailure result = case result of
+  Format.Failure maybeLocation failureReason -> Just (maybeLocation, failureReason)
+  _ -> Nothing
+
+formatFailure :: Maybe Format.Location -> Format.FailureReason -> String
+formatFailure maybeLocation failureReason =
+  mconcat
+    [ "FAILURE ",
+      formatLocation maybeLocation,
+      ": ",
+      formatFailureReason failureReason
+    ]
+
+formatLocation :: Maybe Format.Location -> String
+formatLocation maybeLocation =
+  mconcat
+    [ maybe "?" Format.locationFile maybeLocation,
+      ":",
+      maybe "0" (show . Format.locationLine) maybeLocation,
+      ":",
+      maybe "0" (show . Format.locationColumn) maybeLocation
+    ]
+
+formatFailureReason :: Format.FailureReason -> String
+formatFailureReason failureReason = case failureReason of
+  Format.NoReason -> "no reason"
+  Format.Reason reason -> reason
+  Format.ExpectedButGot prefix expected actual ->
+    mconcat
+      [ maybe "" (<> ": ") prefix,
+        "expected ",
+        expected,
+        " but got ",
+        actual
+      ]
+  Format.Error prefix (Exception.SomeException exception) ->
+    mconcat
+      [ maybe "" (<> ": ") prefix,
+        show (Typeable.typeOf exception),
+        ": ",
+        Exception.displayException exception
+      ]
