3b4047347f65a8acdffee3fd6dbbb1d3699bc576
Taylor Fausak <taylor@fausak.me>
2022-04-15 08:02:45 -0400

    Remove failing test case

--- source/test-suite/Monadoc/Spec.hs
+++ source/test-suite/Monadoc/Spec.hs
@@ -45,7 +45,6 @@ import qualified Test.Hspec as Hspec
 
 spec :: Hspec.Spec
 spec = do
-  Hspec.it "TODO" $ False `Hspec.shouldBe` True
   Monadoc.Action.Blob.UpsertSpec.spec
   Monadoc.Action.Database.InitializeSpec.spec
   Monadoc.Action.Database.VacuumSpec.spec
