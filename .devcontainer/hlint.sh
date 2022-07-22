6fc3ebf0df489369da8a9f6642122ab1dfc146dd
Taylor Fausak <taylor@fausak.me>
2022-07-22 14:43:38 +0000

    2022-07-22

--- .devcontainer/hlint.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env sh
-set -o errexit -o xtrace
-
-if ! command -v hlint 1>&2
-then
-  cabal install hlint
-fi
-
-exec hlint "$@"
--- .devcontainer/ormolu.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env sh
-set -o errexit -o xtrace
-
-if ! command -v ormolu 1>&2
-then
-  cabal install ormolu
-fi
-
-exec ormolu "$@"
