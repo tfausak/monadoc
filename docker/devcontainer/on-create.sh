af7a1abd4f81a35f8b0734b1dba41edd103ec06e
Taylor Fausak <taylor@fausak.me>
2022-08-17 12:37:27 +0000

    Upgrade to GHC 9.4.1

--- docker/devcontainer/on-create.sh
+++ docker/devcontainer/on-create.sh
@@ -7,9 +7,6 @@ cabal --version
 test -d ~/.cabal/packages ||
   cabal --ignore-project update
 
-docker/devcontainer/cabal-shim.sh hlint --version
-docker/devcontainer/cabal-shim.sh ormolu --version
-
 test -f cabal.project.local ||
   cabal configure --enable-tests --flags parallel --jobs --test-show-details direct
 
