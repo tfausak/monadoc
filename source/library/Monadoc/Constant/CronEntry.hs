19f62cfa5683c82d06332ed161beea49a5cf3770
Taylor Fausak <taylor@fausak.me>
2023-05-06 11:46:11 -0500

    Temporarily disable upserting Hackage index

--- source/library/Monadoc/Constant/CronEntry.hs
+++ source/library/Monadoc/Constant/CronEntry.hs
@@ -22,12 +22,6 @@ all =
         CronEntry.task = Task.PruneHackageIndex
       },
     CronEntry.CronEntry
-      { CronEntry.guid = Just . Witch.from $ Uuid.fromWords64 0xf6b75bb337014b13 0xa30acff83b35150b,
-        CronEntry.runAt = Witch.from Time.epoch,
-        CronEntry.schedule = Witch.from Cron.everyMinute,
-        CronEntry.task = Task.UpsertHackageIndex
-      },
-    CronEntry.CronEntry
       { CronEntry.guid = Just . Witch.from $ Uuid.fromWords64 0x0baa03dc275b4cfe 0x8675199ac671132f,
         CronEntry.runAt = Witch.from Time.epoch,
         CronEntry.schedule = Witch.from Cron.everyMinute,
@@ -40,3 +34,12 @@ all =
         CronEntry.task = Task.ProcessUploads
       }
   ]
+
+{-
+    CronEntry.CronEntry
+      { CronEntry.guid = Just . Witch.from $ Uuid.fromWords64 0xf6b75bb337014b13 0xa30acff83b35150b,
+        CronEntry.runAt = Witch.from Time.epoch,
+        CronEntry.schedule = Witch.from Cron.everyMinute,
+        CronEntry.task = Task.UpsertHackageIndex
+      },
+-}
