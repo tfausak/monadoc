c22f5ff88c0dd0717906449fd4397943e7d7243c
Taylor Fausak <taylor@fausak.me>
2024-09-07 21:46:22 -0500

    Prune old passed jobs

--- source/library/Monadoc/Worker/Main.hs
+++ source/library/Monadoc/Worker/Main.hs
@@ -9,6 +9,7 @@ import qualified Monadoc.Action.CronEntry.Prune as CronEntry.Prune
 import qualified Monadoc.Action.CronEntry.Upsert as CronEntry.Upsert
 import qualified Monadoc.Action.Job.Acquire as Job.Acquire
 import qualified Monadoc.Action.Job.Perform as Job.Perform
+import qualified Monadoc.Action.Job.Prune as Job.Prune
 import qualified Monadoc.Action.Job.Release as Job.Release
 import qualified Monadoc.Constant.CronEntry as CronEntry
 import qualified Monadoc.Extra.Exception as Exception
@@ -25,6 +26,7 @@ worker = do
     Exception.handleIf (not . Exception.isAsync) onException
       . Monad.void
       $ Exception.generalBracket Job.Acquire.run Job.Release.run Job.Perform.run
+    Job.Prune.run
 
 onException :: Exception.SomeException -> App.App ()
 onException exception = do
